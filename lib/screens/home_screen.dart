aad69e25d3f2946ee2c81ca19f7b6c4f2ff2af7a
KimJeongJoo <kjjeaus@hufs.ac.kr>
2025-01-31 06:32:30 +0900

    ui 추가

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,6 +11,8 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //main에서 정했던 theme에서 배경색 가져옴
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
       body: Column(
         children: [
           // flexible의 역할은 하드 코딩되는 값을 만들게 해줌
@@ -18,26 +20,69 @@ class _HomeScreenState extends State<HomeScreen> {
           Flexible(
             flex: 1,
             child: Container(
-              decoration: BoxDecoration(
-                color: Colors.red,
+              alignment: Alignment.bottomCenter,
+              child: Text(
+                '25:00',
+                style: TextStyle(
+                  color: Theme.of(context).cardColor,
+                  fontSize: 89,
+                  fontWeight: FontWeight.w600,
+                ),
               ),
             ),
           ),
           Flexible(
             // flex 숫자 크기에 따라 보여지는 화면 비율 달라짐 -> 반응형으로 만들때 용이
             flex: 3,
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.green,
+            child: Center(
+              child: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.play_circle_outline),
+                iconSize: 98,
+                color: Theme.of(context).cardColor,
               ),
             ),
           ),
           Flexible(
             flex: 1,
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).cardColor,
+                    ),
+                    child: Column(
+                      // 주 방향은 수직 방향
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Pomodoros',
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Theme.of(context)
+                                .textTheme
+                                .headlineLarge!
+                                .color,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        Text(
+                          '0',
+                          style: TextStyle(
+                            fontSize: 58,
+                            color: Theme.of(context)
+                                .textTheme
+                                .headlineLarge!
+                                .color,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ],
